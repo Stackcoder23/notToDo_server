4d9500f639ebac956c2e226f4bf3182c0609f6fd
Stackcoder <mjoshi23.mj@gmail.com>
2023-07-18 19:08:00 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const app = express();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the server");
+})
+
 app.get("/user", (req, res) => {
     let db_connect = dbo.getDb();
     db_connect
